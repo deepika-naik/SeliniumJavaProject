07b3e702e0c26496fc5d30c8e4ccf2a0a3160120
Deepa Naik <deepikanaik666@gmail.com>
2023-03-17 07:57:17 +0530

    day04
    Mouse overing,

---
+++ src/day04/Program04.java
@@ -0,0 +1,24 @@
+package day04;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Program04 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver;
+		driver = new EdgeDriver();
+		driver.get("https://demo.automationtesting.in/Alerts.html");		
+		driver.findElement(By.xpath("//a[@href='#CancelTab']")).click();
+		driver.findElement(By.xpath("//button[@class='btn btn-primary']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();//
+		//driver.switchTo().alert().dismiss();//cancel
+	}
+
+}
